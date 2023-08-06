c224e80805e01cb8eeb0961b5c9946c977713766
gonve13 <gonve13@gmail.com>
2023-08-06 18:10:32 -0300

    solucion al problema de bubbling

--- eventos.js
+++ eventos.js
@@ -1,9 +1,16 @@
 window.addEventListener('DOMContentLoaded', () => {
-
+	const boton = document.getElementById("boton");
 	const divBoton = document.getElementById("div_boton");
 
 	divBoton.addEventListener("click", 
 		() => {
 		alert("Hola! Soy el div");
 	});
+	
+	boton.addEventListener("click",
+	(event) => {
+		event.stopPropagation();
+		alert("Hola!");
+	}
+	);
 });
